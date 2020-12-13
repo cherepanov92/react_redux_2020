5358f5d511ffbe7591895048da837d8d165944b2
cherepanov92 <cherepanov92@gmail.com>
2020-12-13 21:45:46 +0500

    [*] Написал стили размеров кнопки

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './style.css';
 
-const Button = ({ children, mode }) => {
+const Button = ({ children, mode, size }) => {
   let modeClassName = mode ? `Button--${mode}` : "";
+  let sizeClassName = size ? `Button--${size}` : "";
   
-  return <button className={`Button ${modeClassName}`}>{children}</button>;
+  return <button className={`Button ${modeClassName} ${sizeClassName}`}>{children}</button>;
+}
+
+Button.defaultProps = {
+  mode: 'primary',
+  size: 'normal',
+}
+
+Button.propTypes = {
+  mode: PropTypes.oneOf(['primary', 'secondary', 'success', 'danger']),
+  size: PropTypes.oneOf(['small', 'normal', 'extra']),
 }
 
 export default Button;
--- src/index.js
+++ src/index.js
@@ -6,10 +6,18 @@ import Button from "./components/Button/Button";
 const App = () => {
   return (
     <div>
-      <Button>Normal</Button>
-      <Button mode={"secondary"}>Secondary</Button>
-      <Button mode={"success"}>Success</Button>
-      <Button mode={"danger"}>Danger</Button>
+      <div>
+        <Button>Normal</Button>
+        <Button mode={'secondary'}>Secondary</Button>
+        <Button mode={'success'}>Success</Button>
+        <Button mode={'danger'}>Danger</Button>
+      </div>
+      <div>
+        <Button>Normal</Button>
+        <Button size={'small'}>small</Button>
+        <Button size={'normal'}>normal</Button>
+        <Button size={'extra'}>extra</Button>
+      </div>
     </div>
   )
 }
