4e2b14d45a4b4e2dfe738c790b180dd8fbbd311e
cherepanov92 <cherepanov92@gmail.com>
2020-12-13 16:55:20 +0500

    [+] Переход на create-react-app

--- index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const text = React.createElement('p',{}, "Первый текст...");
-ReactDOM.render(text, document.getElementById('root'));
